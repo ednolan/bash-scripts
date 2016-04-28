701ff3dd2a71dcba407ec1de33ce9aeb5ec5406c
Edward Nolan <enolan@andrew.cmu.edu>
2016-04-27 22:00:08 -0400

    added resolution change script

---
+++ change-primary-resolution.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# change-primary-resolution.sh
+# Changes the resolution of the primary display using xrandr
+# Usage: ./change-primary-resolution.sh X Y
+#  X: Desired horizontal resolution (multiple of 8)
+#  Y: Desired vertial resolution
+
+# extract primary screen name from xrandr
+PRIMARY_SCREEN=`xrandr | sed -n -e 's/^\(.*\) connected primary.*/\1/p'`
+
+# get mode line from cvt
+MODELINE=`cvt $1 $2 | sed -n -e 's/^.*Modeline //p'`
+if [[ $MODELINE == *usage:* ]] # invalid input to cvt
+then
+  echo "invalid resolution"
+  exit 1
+fi
+
+# mode name is in quotes
+MODE_NAME=`echo $MODELINE | sed -n -e 's/^"\(.*\)".*/\1/p'`
+
+# silence stderr if mode already exists
+xrandr --newmode $MODELINE 2> /dev/null
+xrandr --addmode $PRIMARY_SCREEN $MODE_NAME
+xrandr --output $PRIMARY_SCREEN --mode $MODE_NAME
